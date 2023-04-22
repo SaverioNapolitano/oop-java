addfd9ab0cf2d1e61e2113cd0458982af8563642
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2023-04-22 18:32:27 +0200

    Reformat code

--- src/main/java/com/snapolitano/exercises/functional/AllPalindrome.java
+++ src/main/java/com/snapolitano/exercises/functional/AllPalindrome.java
@@ -3,7 +3,7 @@ package com.snapolitano.exercises.functional;
 import java.util.List;
 
 public class AllPalindrome {
-    public static boolean allPalindrome(List<String> strings){
+    public static boolean allPalindrome(List<String> strings) {
         return strings.stream().allMatch(s -> s.contentEquals(new StringBuilder(s).reverse()));
     }
 }
