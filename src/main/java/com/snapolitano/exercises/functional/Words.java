e258f6307f7b3f30f41b1098c133ea26b20f041a
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2023-04-24 20:38:34 +0200

    Adding Words

---
+++ src/main/java/com/snapolitano/exercises/functional/Words.java
@@ -0,0 +1,24 @@
+package com.snapolitano.exercises.functional;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+public class Words {
+    public static List<String> getLines(String filename) throws IOException {
+        return Files.readAllLines(Path.of(filename));
+    }
+
+    public static List<String> lineToWords(String line) {
+        return Arrays.asList(line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+"));
+    }
+
+    public static long howManyLines(String filename, String subString) throws IOException {
+        return getLines(filename).stream().filter(s -> s.contains(subString)).count();
+    }
+    public static long howManyTimes(String filename, String word) throws IOException {
+        return lineToWords(getLines(filename).toString()).stream().filter(s -> s.contentEquals(word)).count();
+    }
+}
