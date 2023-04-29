85dc156a17e02c8c0defabf3cb956e8601eb0c28
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2023-04-29 14:40:53 +0200

    Add exceptions package + Division

---
+++ src/test/java/com/snapolitano/exercises/exceptions/DivisionTest.java
@@ -0,0 +1,16 @@
+package com.snapolitano.exercises.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionTest {
+
+    @Test
+    void division() {
+        assertThrows(IllegalArgumentException.class, () -> Division.division(-1, 0));
+        assertThrows(IllegalArgumentException.class, () -> Division.division(1, 0));
+        assertEquals(2.0, Division.division(4.0, 2.0), 0.001);
+        assertEquals(-2.0, Division.division(-8.0, 4.0), 0.001);
+    }
+}
