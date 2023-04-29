9c790dec85b2c83903d86d6fc8f7615952329134
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2023-04-29 14:48:22 +0200

    Add DivisionSquared + update

---
+++ src/test/java/com/snapolitano/exercises/exceptions/DivisionSquaredTest.java
@@ -0,0 +1,16 @@
+package com.snapolitano.exercises.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionSquaredTest {
+
+    @Test
+    void divisionSquared() {
+        assertEquals(4, DivisionSquared.divisionSquared(4, 2));
+        assertEquals(4, DivisionSquared.divisionSquared(-8, 4));
+        assertEquals(0, DivisionSquared.divisionSquared(4, 0));
+        assertEquals(0, DivisionSquared.divisionSquared(-8, 0));
+    }
+}
